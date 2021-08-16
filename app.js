8dae292f799e9308a8f47dc140a663fcc7cc502d
Jableed43 <javiernehuenlopez@gmail.com>
2021-08-15 22:38:40 -0300

    Correciones realizadas

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const app = express()
 const path = require('path')
 const port = 3030
 
-/* Middleware */
+/* Middleware, establecer ruta estatica para requerir archivos */
 app.use(express.static('public'))
 
+/* app get */
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
 
 
