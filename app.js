04bb9bd21aa29c36fc3aec5a7003ab045d08f8ad
Jableed43 <javiernehuenlopez@gmail.com>
2021-09-11 00:39:08 -0300

    sign-up

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.use(express.static('public'))
 /* app get */
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
 
+/* sign-up */
+app.get('/sign', (req, res) => res.sendFile(path.join(__dirname, 'views', 'sign-up.html')))
+
 
 /* Servidor escuchando, siempre va al final */
 app.listen(port, () => console.log("Server corriendo en http://localhost:"+port))
